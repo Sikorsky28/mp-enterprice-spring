a482173ad4659ae553e497a1c988f15e24b68b19
Max Sikorsky <pain.maker28@gmail.com>
2026-03-10 19:38:54 +0700

    applie spotless

--- src/test/java/ru/mentee/library/LibraryManagementApplicationTests.java
+++ src/test/java/ru/mentee/library/LibraryManagementApplicationTests.java
@@ -8,8 +8,6 @@ import org.springframework.context.annotation.Import;
 @SpringBootTest
 class LibraryManagementApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+  @Test
+  void contextLoads() {}
 }
